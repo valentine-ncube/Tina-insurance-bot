4e19770c4c58a508c6caf1ae3dd739c7ef1ddb1d
Valentine Ncube <valentinemelly77@gmail.com>
2025-06-10 02:08:50 +1200

    added comments

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import tinaRoutes from './routes/tinaRoutes.js'
 
 dotenv.config()
 
+//middleware
 const app = express()
 const PORT = process.env.PORT || 5000
 
@@ -13,10 +14,12 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use('/api', tinaRoutes)
 
+// Root route
 app.get('/', (req, res) => {
   res.send('Tina the Insurance Assistant is ready to help!')
 })
 
+//404 handler
 app.use((req, res) => {
   res.status(404).send('Endpoint not found')
 })
